d5fd6d2a4d3d0b6e867fdd5e6a3cc8b8eed001a7
Natalia Henao <nataliahenaor24@gmail.com>
2025-08-22 16:36:51 -0500

    feat: add to FileSystem implement mkdir, touch, ls, rm

--- src/main/java/org/assessment/filesystem/FileSystem.java
+++ src/main/java/org/assessment/filesystem/FileSystem.java
@@ -1,5 +1,7 @@
 package org.assessment.filesystem;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileSystem {
     private final Directory root;
@@ -94,6 +96,54 @@ public class FileSystem {
             parent.addItem(new File(fileName, parent));
         } else {
             System.out.println("File already exists: " + fileName);
+        }
+    }
+
+    public void ls() {
+        for (FileSystemItem item : currentDirectory.getChildren()) {
+            System.out.println((item.isDirectory() ? "[DIR] " : "[FILE] ") + item.getName());
+        }
+    }
+
+
+    public void mkdir(String path) {
+        Directory parent = navigateToParent(path);
+        if (parent == null) return;
+
+        String dirName = lastSegment(path);
+        FileSystemItem item = parent.getItem(dirName);
+        if (item == null) {
+            parent.addItem(new Directory(dirName, parent));
+        } else {
+            System.out.println("Directory already exists: " + dirName);
+        }
+    }
+
+    public void pwd() {
+        List<String> path = new ArrayList<>();
+        Directory dir = currentDirectory;
+        while (dir != null) {
+            path.add(0, dir.getName());
+            dir = dir.getParent();
+        }
+        String result = String.join("/", path);
+
+        while (result.startsWith("//")) {
+            result = result.substring(1);
+        }
+        System.out.println(result);
+    }
+
+    public void rm(String path) {
+        Directory parent = navigateToParent(path);
+        if (parent == null) return;
+
+        String itemName = lastSegment(path);
+        FileSystemItem item = parent.getItem(itemName);
+        if (item != null) {
+            parent.removeItem(itemName);
+        } else {
+            System.out.println("File not found: " + itemName);
         }
     }
 
